bae0d910fa3e3c504dacded61f1baacc0e55510d
Wasin-Nop <now.nsw@gmail.com>
2020-04-24 23:25:52 +0700

    Create Exercise4_Wasin_N.py

---
+++ Exercise4_Wasin_N.py
@@ -0,0 +1,9 @@
+feng = "Foundation English"
+gb = "General Business"
+itcs = "Introduction to Computer Systems"
+cp = "Computer Programming"
+print("---Your Score---")
+print(feng,":",75.8,"/100")
+print(gb,":",52.3,"/100")
+print(itcs,":",85.2,"/100")
+print(cp,":",98.6,"/100")
