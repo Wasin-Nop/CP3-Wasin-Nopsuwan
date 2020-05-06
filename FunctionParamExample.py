a9bfb2fb7b5afa983f8a5f04dffbc3d030a84a68
Wasin-Nop <now.nsw@gmail.com>
2020-05-06 10:34:09 +0700

    Create FunctionParamExample.py

---
+++ FunctionParamExample.py
@@ -0,0 +1,12 @@
+def plusNumber(x,y):
+    print(x+y)
+plusNumber(100,25)
+def minusNumber(x,y):
+    print(x-y)
+minusNumber(500,75)
+def multipliedNumber(x,y):
+    print(x*y)
+multipliedNumber(90,3)
+def divideNumber(x,y):
+    print(x/y)
+divideNumber(25,5)
